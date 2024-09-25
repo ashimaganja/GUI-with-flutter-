2beb725fd90d33c0eea5645606970b1b2d2d3755
Ashim Aganja <ashimaganja98@gmail.com>
2024-09-25 13:09:21 -0400

    added a select button and fixed yaml file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-import 'dart:typed_data';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_libserialport/flutter_libserialport.dart';
@@ -12,21 +9,22 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GUI for Available COM Ports',
+      title: 'Flutter Windowns Serial Port Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'All the Available COM Ports'),
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+      home: const MyHomePage(title: 'Flutter Windowns Serial Port Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
+
   final String title;
 
   @override
@@ -35,25 +33,16 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<SerialPort> portList = [];
-  SerialPort?
-      _serialPort; // Can hold an instance of Serial Port or a null value and _serialPort is a private value
+  SerialPort? _serialPort;
   List<Uint8List> receiveDataList = [];
   final textInputCtrl = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    var i = 0;
     for (final name in SerialPort.availablePorts) {
       final sp = SerialPort(name);
-      if (kDebugMode) {
-        print('${++i}) $name');
-        print('\tDescription: ${cp949.decodeString(sp.description ?? '')}');
-        print('\tManufacturer: ${sp.manufacturer}');
-        print('\tSerial Number: ${sp.serialNumber}');
-        print('\tProduct ID: 0x${sp.productId?.toRadixString(16) ?? 00}');
-        print('\tVendor ID: 0x${sp.vendorId?.toRadixString(16) ?? 00}');
-      }
+
       portList.add(sp);
     }
     if (portList.isNotEmpty) {
@@ -70,59 +59,50 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            const BigCard(),
-            Expanded(
-              flex: 1,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  DropdownButton(
-                    value: _serialPort,
-                    items: portList.map((item) {
-                      return DropdownMenuItem(
-                          child: Text(
-                              "${item.name}: ${cp949.decodeString(item.description ?? '')}"),
-                          value: item);
-                    }).toList(),
-                    onChanged: 
-                     Selection,
-                   
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(widget.title),
       ),
-    );
-  }
+      body: SizedBox(
+        height: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+                flex: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    DropdownButton(
+                       value: _serialPort,
+                      items: portList.map((item) {
+                        return DropdownMenuItem(
+                            child: Text(
+                                "${item.name}: ${cp949.decodeString(item.description ?? '')}"),
+                            value: item);
+                      }).toList(),
+                      onChanged: (e) {
+                        setState(() {
+                          changedDropDownItem(e as SerialPort);
+                          debugPrint(e.name);
+                        });
+                      },
+                    ),
+                    const SizedBox(
+                      width: 50.0,
+                    ),
 
-void Selection(e) {
-    setState(() {
-      changedDropDownItem(e as SerialPort);
-    });
-
-    print('You have selected ${e.name}');
-   
-  }
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Card(
-      child: Center(
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Text("Available COM Ports from your device."),
+                    OutlinedButton(
+                      child: Text("Select"),
+                      onPressed: () {
+                        if (_serialPort == null) {
+                          return;
+                        }
+                        
+                      },
+                    ),
+                  ],
+                ))
+          ],
         ),
       ),
     );
